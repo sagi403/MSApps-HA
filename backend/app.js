7bfdebeabd98a12b5281371cab868ae530582083
Sagi <sagi403@gmail.com>
2023-06-01 15:08:09 +0300

    Adding error middleware

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ import helmet from "helmet";
 import cors from "cors";
 
 import imageRoutes from "./routes/imageRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 const app = express();
 
@@ -19,5 +20,8 @@ app.use("/api/images", imageRoutes);
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 export { app };
