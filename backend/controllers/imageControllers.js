120a65e12358613dd2b55adb6d5761f4296ad6c6
Sagi <sagi403@gmail.com>
2023-06-01 15:29:27 +0300

    Getting the images url with get request

--- backend/controllers/imageControllers.js
+++ backend/controllers/imageControllers.js
@@ -11,9 +11,20 @@ const getImages = asyncHandler(async (req, res) => {
   const page = +req.query.pageNumber || 1;
   const category = req.query.pageNumber || "category";
 
-  const { data } = await pixabayApi.get(`/?key=${keys.apiKey}&q=${category}`);
+  const { data } = await pixabayApi.get(
+    `/?key=${keys.apiKey}&q=${category}&per_page=${pageSize}&page=${page}`
+  );
 
-  res.json(data);
+  const imagesDetails = data.hits.sort((a, b) => a.id - b.id);
+
+  const images = imagesDetails.map(image => image.webformatURL);
+
+  if (images.length !== 0) {
+    res.json({ images });
+  } else {
+    res.status(404);
+    throw new Error("There are no images available");
+  }
 });
 
 export { getImages };
